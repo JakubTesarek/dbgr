343859da2bc7d79cba5425441e842c5de7a25380
Jakub Tesárek <mail@jakubtesarek.cz>
2019-05-17 14:47:27 +0200

    add support for optional name of requests

--- dbgr/__init__.py
+++ dbgr/__init__.py
@@ -1,9 +1,17 @@
 import functools
 from dbgr.requests import register_request
 
-def request(request):
-    @functools.wraps(request)
-    async def wrapper_decorator(*args, **kwargs):
-        await request(*args, **kwargs)
-    register_request(wrapper_decorator)
-    return wrapper_decorator
+
+def request(name=None):
+    func = name
+    if callable(func):
+        register_request(func)
+        return func
+    else:
+        @functools.wraps(func)
+        def decorator(func):
+            if name is not None:
+                func.__name__ = name
+            register_request(func)
+            return func
+        return decorator
