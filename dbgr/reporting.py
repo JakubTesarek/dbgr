9bee614d84c4141580f050a84474a6748de0f435
Jakub Tesárek <mail@jakubtesarek.cz>
2019-05-17 23:36:51 +0200

    color code response

--- dbgr/reporting.py
+++ dbgr/reporting.py
@@ -1,9 +1,10 @@
 import aiohttp
 import asyncio
-import colorama
 from datetime import datetime, timedelta
 from itertools import cycle
-
+from pygments import highlight, lexers, formatters
+import json
+from colorama import Style, Fore
 
 class ProgressBar():
     def start(self):
@@ -41,14 +42,38 @@ class Reporter():
         tracer.on_request_redirect.append(self.on_request_redirect)
         return tracer
 
-    async def report_request_finished(self, session, trace_config_ctx, params):
-        colorama.init(autoreset=True)
-        response = params.response
-        print(f'< {response.method} {response.url}')
-        print(f'< {response.status} {response.reason}')
-        print('< Headers:')
+    async def print_request(self, response):
+        print(f'{Style.DIM}>{Style.RESET_ALL} {Style.BRIGHT}{response.method} {response.url}')
+
+    async def print_info(self, response):
+        if response.status < 400:
+            color = Fore.GREEN
+        else:
+            color = Fore.RED
+        print(f'{Style.DIM}<{Style.RESET_ALL} {color}{response.status} {response.reason}')
+
+    async def print_headers(self, response):
+        print(f'{Style.DIM}<')
+        print(f'{Style.DIM}<{Style.RESET_ALL} {Style.BRIGHT}Headers:')
         for name, value in response.headers.items():
-            print(f'<  {name}: {value}')
+            print(f'{Style.DIM}<{Style.RESET_ALL}  {name}: {value}')
+
+    async def print_response(self, response):
+        if 'application/json' in response.headers['Content-Type']:
+            print(f'{Style.DIM}<')
+            print(
+                f'{Style.DIM}<{Style.RESET_ALL} {Style.BRIGHT}Content{Style.RESET_ALL}'
+                f'{Style.DIM}(application/json){Style.RESET_ALL}:'
+            )
+            output = json.dumps(await response.json(), sort_keys=True, indent=2)
+            print(highlight(output, lexers.JsonLexer(), formatters.TerminalFormatter()))
+
+    async def report_request_finished(self, session, trace_config_ctx, params):
+        response = params.response
+        await self.print_request(response)
+        await self.print_info(response)
+        await self.print_headers(response)
+        await self.print_response(response)
 
     async def on_request_redirect(self, session, trace_config_ctx, params):
         print(f'Redirect -> {params.url}')
