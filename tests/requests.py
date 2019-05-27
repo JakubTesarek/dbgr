7e2088f161b2c4ec0c474697cabfe8a011cc65a2
Jakub Tesárek <mail@jakubtesarek.cz>
2019-05-27 16:49:29 +0200

    Test get_requests

--- tests/requests.py
+++ tests/requests.py
@@ -1,4 +1,5 @@
-from dbgr.requests import parse_cmd_arguments
+import dbgr.requests
+from dbgr.requests import parse_cmd_arguments, get_requests
 
 
 def test_parse_cmd_arguments_empty():
@@ -12,3 +13,14 @@ def test_parse_cmd_arguments():
 
 def test_cmd_arguments_without_value_resolved_to_true():
     assert parse_cmd_arguments(['arg1']) == {'arg1': True}
+
+
+def test_get_requests_loads_requests_once(monkeypatch):
+    def mocked_load_requests():
+        mocked_load_requests.counter += 1
+        monkeypatch.setattr(dbgr.requests, '_REQUESTS', {'module': {}})
+    mocked_load_requests.counter = 0
+    monkeypatch.setattr(dbgr.requests, 'load_requests', mocked_load_requests)
+    assert get_requests() == {'module': {}}
+    assert get_requests() == {'module': {}}
+    assert mocked_load_requests.counter == 1
