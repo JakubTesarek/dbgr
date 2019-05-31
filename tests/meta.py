1fae45cf9759bd409950f3f6ce73b7c9afbd182a
Jakub Tesárek <mail@jakubtesarek.cz>
2019-05-31 14:39:34 +0200

    Fix regex deprecation warinings

--- tests/meta.py
+++ tests/meta.py
@@ -3,4 +3,4 @@ from dbgr.meta import __version__
 
 
 def test_version_format():
-    assert re.match('^[1-9]\.(0|([1-9]\d*))\.(0|([1-9]\d*))$', __version__)
+    assert re.match(r'^[1-9]\.(0|([1-9]\d*))\.(0|([1-9]\d*))$', __version__)
--- tests/tools.py
+++ tests/tools.py
@@ -4,7 +4,7 @@ from dbgr.tools import uuid4
 
 def test_uuid4():
     uuids = set()
-    regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+    regex = re.compile(r'^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}$', re.I)
     for i in range(1000):
         uuid = uuid4()
         assert bool(regex.match(uuid))
